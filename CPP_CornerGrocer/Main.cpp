7781660f0bb79db732e60311ac385565450f167a
Lawrence <lawrence.artl@gmail.com>
2021-06-07 08:10:43 -0700

    Added CornerGrocer project

---
+++ CPP_CornerGrocer/Main.cpp
@@ -0,0 +1,215 @@
+#include <Python.h>
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName)
+{
+	char* procname = new char[pName.length() + 1];
+	std::strcpy(procname, pName.c_str());
+
+	Py_Initialize();
+	PyObject* my_module = PyImport_ImportModule("PythonCode");
+	PyErr_Print();
+	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+	PyObject* my_result = PyObject_CallObject(my_function, NULL);
+	Py_Finalize();
+
+	delete[] procname;
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	char* paramval = new char[param.length() + 1];
+	std::strcpy(paramval, param.c_str());
+
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(z)", paramval);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+	delete[] paramval;
+
+
+	return _PyLong_AsInt(presult);
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(i)", param);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+
+	return _PyLong_AsInt(presult);
+}
+char Menu() {
+	//create a menu for the user
+	char select;
+	cout << "\nMAIN MENU" << "\n==============" << endl;
+	cout << "1: Print a Multiplication Table" << endl;
+	cout << "2: Double a Value" << endl;
+	cout << "3: ReadFile" << endl;
+	cout << "4: Quit" << endl;
+	cout << "Please enter your selection and press 'enter'..." << endl;
+	cin >> select;
+	//return the selected value for checking
+	return select;
+}
+
+int Checker() {
+	//generic function to intput an integer and check it
+	int choice;
+	//clean the screan
+	system("cls");
+	cout << "Please enter an integer: " << endl;
+	cin >> choice;
+	//check that the input is appropriate
+	while (cin.fail()) {
+		cout << "Invalid input, please enter an integer only" << endl;
+		cin.clear();				//clears whatever was .cin
+		cin.ignore(100, '\n');		//ignores whatever was input up to 100 characters
+		cin >> choice;
+	}
+	//clean the screen and clear cin so that any extra values don't pass to the menu afterwards
+	system("cls");
+	cin.clear();
+	cin.ignore(100, '\n');
+	return choice;
+}
+
+
+void main()
+{
+	//CallProcedure("printsomething");
+	//cout << callIntFunc("PrintMe", "House") << endl;
+	//cout << callIntFunc("SquareValue", 2);
+
+	char select;
+	int choice;
+
+	do {
+		select = Menu();
+		switch (select) {
+		case '1': {
+			choice = Checker();
+			callIntFunc("MultiplicationTable", choice);
+			break;
+		}
+		case '2': {
+			choice = Checker();
+			int y = callIntFunc("DoubleValue", choice);
+			cout << "You entered " << choice << " and it's value doubled is " << y << endl;
+			break;
+
+		}
+		case '3':
+			//choice = Checker();
+			CallProcedure("ReadFile");
+			break;
+
+		default: {
+			cout << "Program end.\n" << endl;
+			break;
+		}
+		}
+
+
+
+	} while (select != '4');
+
+}
