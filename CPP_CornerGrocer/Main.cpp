3c5faa1f6c13f77624469a093932ba2136ddbc2c
Lawrence <lawrence.artl@gmail.com>
2021-06-08 21:22:04 -0700

    Finished Project 3 Corner Grocer

--- CPP_CornerGrocer/Main.cpp
+++ CPP_CornerGrocer/Main.cpp
@@ -3,8 +3,112 @@
 #include <Windows.h>
 #include <cmath>
 #include <string>
+#include <fstream>
+#include <iomanip>
+
+#include "Menu.h"
 
 using namespace std;
+
+
+
+void GraphData() {
+	ifstream inFS;
+	ifstream inNow;
+	string item;
+	int count;
+	string graph = "";		//histograph string
+	inFS.open("frequency.dat");
+
+	//try to open the file, otherwise return to main
+	if (!inFS.is_open()) {
+		cout << "Could not open file." << endl;
+		return;
+	}
+	
+	//print the graph
+	cout << " HISTOGRAM OF ITEMS PURCHASED" << endl;
+	cout << " =============================\n" << endl;
+	do {
+		inFS >> item;
+		if (item == "end") {	//make sure python func prints 'end' at the end so that this works
+								//otherwise 'endof' doesn't work and prints the last item twice
+								//doesn't seem to be a simple workaround using 'endof'
+			return;
+		}
+		inFS >> count;
+		for (int i = 0; i < count; i++) {
+			graph += "@";
+		}
+
+		cout << setw(12) << right << item << "|" << graph << " " << count << endl;
+		graph = "";
+		
+	} while (inFS);
+}
+
+string SearchItem(Menu& newMenu) {
+	//handles the search function to get string to pass to python
+	system("cls");
+	string item;
+	newMenu.SetNewCursor(0, 0);
+	cout << "Enter an item to search for: ";
+	cin >> item;
+	return item;
+}
+
+void DrawTitle(Menu& mainMenu) {
+	//draw a splash screen!
+	string splash[6];
+	splash[0] = "   ____                              ____                         ";
+	splash[1] = "  / ___|___  _ __ _ __   ___ _ __   / ___|_ __ ___   ___ ___ _ __ ";
+	splash[2] = " | |   / _ \\| '__| '_ \\ / _ \\ '__| | |  _| '__/ _ \\ / __/ _ \\ '__|";
+	splash[3] = " | |__| (_) | |  | | | |  __/ |    | |_| | | | (_) | (_|  __/ |   ";
+	splash[4] = "  \\____\\___/|_|  |_| |_|\\___|_|     \\____|_|  \\___/ \\___\\___|_|   ";
+	splash[5] = "                                                                  ";
+	//draw the left margin!	
+	mainMenu.SetNewCursor(1, 0);
+	cout << "|=============================|\n" << endl;
+
+	int row = 1;
+	for (int i = 0; i < 6; i++) {
+		mainMenu.SetNewCursor(row, 30);
+		cout << splash[i] << endl;
+		row++;
+	}
+	for (int i = 0; i < 25; i++) {
+		mainMenu.SetNewCursor(i, 0);
+		cout << "|";
+		mainMenu.SetNewCursor(i, 30);
+		cout << "|";
+	}
+	mainMenu.SetNewCursor(25, 0);
+	cout << "|=============================|\n" << endl;
+}
+
+void ClearColumn(Menu& newMenu) {
+	//clear the left column 
+	//this avoids clearing the whole screen, reduces flashing
+	
+	string blank = "";
+	for (int i = 0; i < 31; i++) {
+		blank += " ";
+	}
+
+	for (int i = 0; i < 30; i++) {
+		newMenu.SetNewCursor(i, 0);
+		cout << blank;
+
+	}
+}
+
+int MainMenu(Menu& newMenu, vector<string>& menuItems) {
+	//handles main menu selection
+	DrawTitle(newMenu);
+	int selection = 0;
+	newMenu.RunMenu(menuItems, selection);
+	return selection;
+}
 
 /*
 Description:
@@ -137,79 +241,82 @@ int callIntFunc(string proc, int param)
 
 	return _PyLong_AsInt(presult);
 }
-char Menu() {
-	//create a menu for the user
-	char select;
-	cout << "\nMAIN MENU" << "\n==============" << endl;
-	cout << "1: Print a Multiplication Table" << endl;
-	cout << "2: Double a Value" << endl;
-	cout << "3: ReadFile" << endl;
-	cout << "4: Quit" << endl;
-	cout << "Please enter your selection and press 'enter'..." << endl;
-	cin >> select;
-	//return the selected value for checking
-	return select;
-}
-
-int Checker() {
-	//generic function to intput an integer and check it
-	int choice;
-	//clean the screan
-	system("cls");
-	cout << "Please enter an integer: " << endl;
-	cin >> choice;
-	//check that the input is appropriate
-	while (cin.fail()) {
-		cout << "Invalid input, please enter an integer only" << endl;
-		cin.clear();				//clears whatever was .cin
-		cin.ignore(100, '\n');		//ignores whatever was input up to 100 characters
-		cin >> choice;
-	}
-	//clean the screen and clear cin so that any extra values don't pass to the menu afterwards
-	system("cls");
-	cin.clear();
-	cin.ignore(100, '\n');
-	return choice;
-}
-
 
 void main()
 {
-	//CallProcedure("printsomething");
-	//cout << callIntFunc("PrintMe", "House") << endl;
-	//cout << callIntFunc("SquareValue", 2);
+	
+	Menu mainMenu;
+	int selection = 0;
+	//modify the menu to look nice
+	vector <string> menuItems = { "List all Items", "Search for an Item", "Print Graph", "Quit" };
+	mainMenu.MenuModifier(menuItems);
 
-	char select;
-	int choice;
+	string instr[3] = { "Select an item using" , "the arrow keys and" , "press 'enter'..." };
+	
+	mainMenu.HideCursorBlink();
 
-	do {
-		select = Menu();
-		switch (select) {
-		case '1': {
-			choice = Checker();
-			callIntFunc("MultiplicationTable", choice);
+	//show the instructions
+	int row = 8;
+	for (int i = 0; i < 3; i++) {
+		mainMenu.SetNewCursor(row, 3);
+		cout << instr[i];
+		row++;
+	}
+	
+	bool run = true;
+	
+	while (run) {
+		selection = MainMenu(mainMenu, menuItems);
+		switch (selection) {
+		case 1: {
+			ClearColumn(mainMenu);
+			mainMenu.SetNewCursor(0, 0);
+			CallProcedure("ListItems");
+			cout << "\n";
 			break;
 		}
-		case '2': {
-			choice = Checker();
-			int y = callIntFunc("DoubleValue", choice);
-			cout << "You entered " << choice << " and it's value doubled is " << y << endl;
+		case 2: {
+			string s = SearchItem(mainMenu); 
+			system("cls");
+			int count = callIntFunc("SingleItem", s);
+			if (count == 0) {
+				cout << "No such item exists, try again." << endl;
+
+			}
+			else {
+				cout << "The number of " << s << " sold today: " << count << "\n\n";
+			}
+			
+			system("pause");
+			system("cls");
+			int row = 8;
+			for (int i = 0; i < 3; i++) {
+				mainMenu.SetNewCursor(row, 3);
+				cout << instr[i];
+				row++;
+			}
 			break;
 
 		}
-		case '3':
-			//choice = Checker();
-			CallProcedure("CountItems");
+		case 3:
+			ClearColumn(mainMenu);
+			mainMenu.SetNewCursor(0, 0);
+			GraphData();
+			break;
+
+		case 4:
+			system("cls");
+			run = false;
 			break;
 
 		default: {
-			cout << "Program end.\n" << endl;
+			
 			break;
 		}
 		}
-
-
-
-	} while (select != '4');
-
+		
+		
+		
+	}
+		
 }
---
+++ CPP_CornerGrocer/Menu.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <ctime>
+#include <conio.h>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
+using namespace std;
+
+class Menu
+{
+public:
+
+
+	int CheckKeyPress();
+	void DrawMenu(string[], int, int);
+	void DrawMenu(vector<string>, int&);
+	void SetNewCursor(int row, int col);
+	void HideCursorBlink();
+	void RunMenu(vector<string>, int&);
+	void MenuModifier(vector<string>& menu); 
+	void SetWindowSize(int w, int h);
+	int GetWindowWidth();
+
+
+
+};
+
